b7cb7b74a65b89e58d1ec5b3fd91e144f4f49d7f
NikaBidzo22 <118376908+NikaBidzo22@users.noreply.github.com>
2023-11-16 11:28:17 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+ScrollReveal({ 
+    reset: true,
+    distance: '200px',
+    duration: 2000,
+    delay: 200 
+});
+
+ScrollReveal().reveal('.title', { origin: 'top'});  
+ScrollReveal().reveal('.mainText', { origin: 'bottom'});  
